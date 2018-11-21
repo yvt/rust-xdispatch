399c65f1a8ee7e390d6c5c2f568f586dd1c7f7b7
yvt <i@yvt.jp>
2018-11-21 18:02:40 +0900

    Add doctests

--- xdispatch/src/lib.rs
+++ xdispatch/src/lib.rs
@@ -1,5 +1,3 @@
-//! # xdispatch for Rust
-//!
 //! This crate reexports items from [`dispatch`]. TODO: In order to make it
 //! available to other platforms than macOS/iOS, it links `xdispatch-core`,
 //! which includes a statically linked version of [XDispatch], a cross-platform
@@ -9,6 +7,36 @@
 //! [`dispatch`]: https://crates.io/crates/dispatch
 //! [XDispatch]: http://opensource.mlba-team.de/xdispatch/
 //!
+//! # Examples
+//!
+//! Below are some examples from `dispatch`:
+//!
+//! ```
+//! use xdispatch::{Queue, QueueAttribute};
+//!
+//! let queue = Queue::create("com.example.rust", QueueAttribute::Serial);
+//! queue.async(|| println!("Hello"));
+//! queue.async(|| println!("World"));
+//! ```
+//!
+//! ```
+//! use xdispatch::{Queue, QueuePriority};
+//!
+//! let queue = Queue::global(QueuePriority::Default);
+//!
+//! let mut nums = vec![1, 2];
+//! queue.foreach(&mut nums, |x| *x += 1);
+//! assert!(nums == [2, 3]);
+//!
+//! let nums = queue.map(nums, |x| x.to_string());
+//! assert!(nums[0] == "2");
+//! ```
+//!
+//! <!--
+//!     The true purpose of including doctests here is to make sure `xdispatch`
+//!     works when linked as a part of dylib output. As opposed to normal tests,
+//!     doctests are compiled in this way.
+//! -->
 extern crate dispatch;
 
 #[doc(no_inline)]
